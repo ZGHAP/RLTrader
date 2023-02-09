6489e77598bbb613d60a17e5577e9656275a2904
Zhaoming <32124379+ZGHAP@users.noreply.github.com>
2023-02-09 15:17:05 +0800

    Update IncrementalProfit.py

--- lib/env/reward/IncrementalProfit.py
+++ lib/env/reward/IncrementalProfit.py
@@ -26,9 +26,12 @@ class IncrementalProfit(BaseRewardStrategy):
         curr_balance = account_history['balance'].values[-1]
         prev_balance = account_history['balance'].values[-2] if len(account_history['balance']) > 1 else curr_balance
 
+        #此为非卖空模型
+        #当前balance大于前一个,必然为平仓所得,reward为worth[-1] - 买入点worh
         if curr_balance > prev_balance:
             reward = net_worths[-1] - net_worths[self.last_bought]
             self.last_sold = current_step
+        #当前balance小于前一个,必然为开仓过程,reward为平仓点是否高于当前价,诱导agent在高位平仓
         elif curr_balance < prev_balance:
             reward = observations['Close'].values[self.last_sold] - current_price()
             self.last_bought = current_step
